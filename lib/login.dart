b2df2e3f334cef500c7adb52c4d00009ca489937
Daniel <danielt.tman@gmail.com>
2020-08-04 16:47:53 +1000

    change login and register to forms

--- lib/login.dart
+++ lib/login.dart
@@ -1,115 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:strydeapp/welcome.dart';
 import 'connector.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Login extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Heading
-          Text(
-            "Login",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xFF409ded),
-              fontSize: 48,
-              fontWeight: FontWeight.bold,),
-          ),
-
-          // Margin
-          const SizedBox(height: 30),
-          // TextFields
-          Container(
-              width: 300,
-              child: Column(children: [
-                // Email TextField
-                TextField(
-                  decoration: InputDecoration(
-                    fillColor: Color(0xFFf0f0f0),
-                    filled: true,
-                    contentPadding:
-                    const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-                ),
-
-                // Password TextField
-                const SizedBox(height: 15),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Color(0xFFf0f0f0),
-                    filled: true,
-                    contentPadding:
-                    const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ])),
-
-          // Buttons
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Back Button
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(10.0)),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Welcome()));
-                },
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: new BorderRadius.circular(10.0),
-                    color: Color(0xFF409ded),
-                  ),
-                  width: 130,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
-                  child: const Text('Back', style: TextStyle(fontSize: 20)),
+        body: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              // Heading
+              Text(
+                "Login",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF409ded),
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Margin
+                    const SizedBox(height: 30),
+                    // TextFields
+                    Container(
+                        width: 300,
+                        child: Column(children: [
+                          // Email TextField
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              fillColor: Color(0xFFf0f0f0),
+                              filled: true,
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
+                            ),
+                          ),
 
-              // Login Button
-              const SizedBox(width: 20),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(10.0)),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Connector(0)));
-                },
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: new BorderRadius.circular(10.0),
-                    color: Color(0xFF409ded),
-                  ),
+                          // Password TextField
+                          const SizedBox(height: 15),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              fillColor: Color(0xFFf0f0f0),
+                              filled: true,
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ])),
 
-                  width: 130,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
-                  child: const Text('Log In', style: TextStyle(fontSize: 20)),
+                    // Buttons
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Back Button
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(10.0)),
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => Welcome()));
+                          },
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: new BorderRadius.circular(10.0),
+                              color: Color(0xFF409ded),
+                            ),
+                            width: 130,
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                            child: const Text('Back', style: TextStyle(fontSize: 20)),
+                          ),
+                        ),
+
+                        // Login Button
+                        const SizedBox(width: 20),
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(10.0)),
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => Connector(0)));
+                          },
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: new BorderRadius.circular(10.0),
+                              color: Color(0xFF409ded),
+                            ),
+                            width: 130,
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                            child: const Text('Log In', style: TextStyle(fontSize: 20)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 80),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 80),
-        ],
-      ),
+              )
+            ])
+        )
     );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -116,183 +116,196 @@ class RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    final TextEditingController _nameController = TextEditingController();
     final TextEditingController _emailController = TextEditingController();
     final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _confirmPasswordController = TextEditingController();
+    final TextEditingController _historyController = TextEditingController();
+
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
     bool _success;
     String _userEmail;
 
     return Scaffold(
       body: Center(
-      child: ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(20.0),
-        children: [
-          Column(
-            children: [
-              // Heading
-              Text(
-                "Sign-Up",
-                style: TextStyle(
-                  color: Color(0xFF409ded),
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: [
+            // Heading
+            Text(
+              "Sign-Up",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFF409ded),
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            // Margin
+            const SizedBox(height: 30),
 
-              // Margin
-              const SizedBox(height: 30),
-              // Name TextField
-              Container(
-                width: 300,
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Color(0xFFf0f0f0),
-                    filled: true,
-                    contentPadding:
-                        const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                    border: OutlineInputBorder(),
-                    labelText: 'Name',
-                  ),
-                ),
-              ),
-
-              // DropDownBoxes
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            Form(
+              key: _formKey,
+              child: Column(
                 children: [
-                  AgeDropdown(),
-                  const SizedBox(width: 30),
-                  GenderDropdown(),
+                  // Name TextField
+                  Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        fillColor: Color(0xFFf0f0f0),
+                        filled: true,
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                        border: OutlineInputBorder(),
+                        labelText: 'Name',
+                      ),
+                    ),
+                  ),
+
+                  // DropDownBoxes
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AgeDropdown(),
+                      const SizedBox(width: 30),
+                      GenderDropdown(),
+                    ],
+                  ),
+
+                  // TextFields
+                  Container(
+                      width: 300,
+                      child: Column(children: [
+                        // Email TextField
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xFFf0f0f0),
+                            filled: true,
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                        ),
+
+                        // Password TextField
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xFFf0f0f0),
+                            filled: true,
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+
+                        // Confirm Password TextField
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          controller: _confirmPasswordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xFFf0f0f0),
+                            filled: true,
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                            border: OutlineInputBorder(),
+                            labelText: 'Confirm Password',
+                          ),
+                        ),
+                      ])),
+
+                  // History of Injury TextField
+                  const SizedBox(height: 15),
+                  Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: _historyController,
+                      maxLines: 4,
+                      decoration: InputDecoration(
+                        fillColor: Color(0xFFf0f0f0),
+                        filled: true,
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                        border: OutlineInputBorder(),
+                        labelText: 'History of Injury',
+                      ),
+                    ),
+                  ),
+
+                  // Buttons
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Back Button
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Welcome()));
+                        },
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            color: Color(0xFF409ded),
+                          ),
+                          width: 130,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                          child: const Text('Back', style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+
+                      // Confirm Button
+                      const SizedBox(width: 20),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          // Sign Up button is pressed, grab information here
+                          print('Email: ' + _emailController.text);
+                          print('Password: ' + _passwordController.text);
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Firstrun()));
+                        },
+                        textColor: Colors.white,
+                        padding: const EdgeInsets.all(0.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            color: Color(0xFF409ded),
+                          ),
+                          width: 130,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                          child:
+                              const Text('Sign Up', style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 80),
                 ],
-              ),
-
-              // TextFields
-              Container(
-                  width: 300,
-                  child: Column(children: [
-                    // Email TextField
-                    const SizedBox(height: 15),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        fillColor: Color(0xFFf0f0f0),
-                        filled: true,
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                      ),
-                    ),
-
-                    // Password TextField
-                    const SizedBox(height: 15),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        fillColor: Color(0xFFf0f0f0),
-                        filled: true,
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                    ),
-
-                    // Confirm Password TextField
-                    const SizedBox(height: 15),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        fillColor: Color(0xFFf0f0f0),
-                        filled: true,
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                        border: OutlineInputBorder(),
-                        labelText: 'Confirm Password',
-                      ),
-                    ),
-                  ])),
-
-              // History of Injury TextField
-              const SizedBox(height: 15),
-              Container(
-                width: 300,
-                child: TextField(
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    fillColor: Color(0xFFf0f0f0),
-                    filled: true,
-                    contentPadding:
-                        const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                    border: OutlineInputBorder(),
-                    labelText: 'History of Injury',
-                  ),
-                ),
-              ),
-
-              // Buttons
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Back Button
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Welcome()));
-                    },
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: new BorderRadius.circular(10.0),
-                        color: Color(0xFF409ded),
-                      ),
-                      width: 130,
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
-                      child: const Text('Back', style: TextStyle(fontSize: 20)),
-                    ),
-                  ),
-
-                  // Confirm Button
-                  const SizedBox(width: 20),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                    onPressed: () {
-                      // Sign Up button is pressed, grab information here
-                      print('Email: ' + _emailController.text);
-                      print('Password: ' + _passwordController.text);
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Firstrun()));
-                    },
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: new BorderRadius.circular(10.0),
-                        color: Color(0xFF409ded),
-                      ),
-                      width: 130,
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
-                      child:
-                          const Text('Sign Up', style: TextStyle(fontSize: 20)),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 80),
-            ],
-          )
-        ],
-      ),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
